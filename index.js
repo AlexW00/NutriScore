0e5dbcdba2bc8cd521a9835c640abf6e8119a44e
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2022-01-17 12:20:53 +0100

    index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { format } from "https://deno.land/std@0.91.0/datetime/mod.ts";
-
 import pogo from "https://deno.land/x/pogo/main.ts";
 
 const server = pogo.server({ port: 8000 });
@@ -7,6 +6,11 @@ const server = pogo.server({ port: 8000 });
 server.router.get("/", () => {
   sendDiscordMessage();
   return "Hello, world!";
+});
+
+server.router.get("/getQuestionnaire/", () => {
+  sendDiscordMessage();
+  return "hi, world!";
 });
 
 server.start();
