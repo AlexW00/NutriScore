4c42389d07a7c2cf117765ed6ab81578b293da27
Quietscher <76123025+Quietscher@users.noreply.github.com>
2022-02-03 12:17:52 +0100

    MainTaskView.js

--- client/js/views/mainTaskSurveyPage/MainTaskView.js
+++ client/js/views/mainTaskSurveyPage/MainTaskView.js
@@ -19,24 +19,6 @@ export default class MainTaskView extends View {
     // }
 
     _render() {
-        // this.$template = document.querySelector("#snippet").content.cloneNode(true);
-        // this.$root = this.$template.querySelector(".result");
-        // this.$root.setAttribute("data-id", this.data.id);
-
-        // this.crediScore = new CrediScoreView(this.data.crediScore);
-        // this.$crediScore = this.crediScore.html();
-        // this.$root.appendChild(this.$crediScore);
-
-        // this.$content = $root.querySelector(".snippet");
-        // this.$content.querySelector(".url").innerHTML = this.data.url;
-        // this.$content.querySelector(".title").innerHTML = this.data.title;
-        // this.$content.querySelector(".info").innerHTML = this.data.info;
-
-        // this.likertScale = new LikertScaleView(this.data.likertScale);
-        // this.likertScale.addEventListener(LikertScaleView.EVENT_LIKERT_CLICKED, this.onLikertScalaClicked);
-        // this.$likertScale = this.likertScale.html();
-        // this.$content.appendChild(this.$likertScale);
-
 
         return this.$root;
     }
