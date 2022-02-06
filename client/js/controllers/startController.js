e00183646c065eff6f5b77dbd187137665c85c85
Quietscher <76123025+Quietscher@users.noreply.github.com>
2022-02-06 13:50:02 +0100

    fixed Umlaut problem

--- client/js/controllers/startController.js
+++ client/js/controllers/startController.js
@@ -8,18 +8,25 @@ import IndexedDbStorageProvider from "../storage/IndexedDbStorageProvider.js";
 
 // ~~~~~~~~ example showcase code ~~~~~~~~ //
 
-const exampleView = new ExampleView({ buttonValue: 1 }); // create a new view
-exampleView.addEventListener("buttonClicked", (event) => {
-  const view = event.view; // retrieve the view instance that triggered the event
+// const exampleView = new ExampleView({ buttonValue: 1 }); // create a new view
+// exampleView.addEventListener("buttonClicked", (event) => {
+//   const view = event.view; // retrieve the view instance that triggered the event
 
-  // usually interaction with the Model would be done here
-  const newButtonValue = view.data.buttonValue + 1;
+//   // usually interaction with the Model would be done here
+//   const newButtonValue = view.data.buttonValue + 1;
 
-  // update the view
-  view.data.buttonValue = newButtonValue;
-  view.$button.innerHTML = newButtonValue;
-});
+//   // update the view
+//   view.data.buttonValue = newButtonValue;
+//   view.$button.innerHTML = newButtonValue;
+// });
 
-document.body.appendChild(exampleView.html()); // append the new view to the body
+// document.body.appendChild(exampleView.html()); // append the new view to the body
 
 // Storage db testing
+
+const startSurveyButton = document.querySelector(".button-next");
+startSurveyButton.addEventListener("click", onStartSurveyClicked);
+
+function onStartSurveyClicked() {
+  window.location = window.location + "survey";
+}
